da2c91a4609e9b8b70973d97704885232b60cbb0
Durga prasad <Durga prasad@LAPTOP-LN9V9DGH>
2022-10-14 22:27:35 +0530

    added name

--- src/cseb1/Demo1.java
+++ src/cseb1/Demo1.java
@@ -5,6 +5,7 @@ public class Demo1 {
 	public static void main(String[] args) {
 		
 		System.out.println("WELCOME TO ACOE COLLEGE from suramplame");
+		System.out.println("DURGA PRASAD");
 		// TODO Auto-generated method stub
 
 	}

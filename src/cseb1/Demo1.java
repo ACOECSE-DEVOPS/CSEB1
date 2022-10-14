a03fe5fca6e7c2b2335b5a6eb64f80db95429e6b
Durga prasad <Durga prasad@LAPTOP-LN9V9DGH>
2022-10-14 11:39:19 +0530

    First Commit

---
+++ src/cseb1/Demo1.java
@@ -0,0 +1,12 @@
+package cseb1;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("WELCOME TO DEVOPS LAB");
+		// TODO Auto-generated method stub
+
+	}
+
+}
